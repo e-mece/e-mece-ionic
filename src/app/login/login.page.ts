82096ebdbe8f788003100544893ff45b01d38c50
Ahmet Uysal <auysal16@ku.edu.tr>
2019-09-08 02:04:09 +0300

    Updated login UI

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+  styleUrls: ['./login.page.scss']
 })
 export class LoginPage implements OnInit {
+  credentials: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly router: Router,
+    private readonly formBuilder: FormBuilder
+  ) {
+    if (this.authService.hasCurrentUser()) {
+      this.router.navigate(['/tabs']);
+    }
   }
 
+  ngOnInit() {
+    this.credentials = this.formBuilder.group({
+      identifier: new FormControl('', { validators: [Validators.required] }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(8)]
+      })
+    });
+  }
+
+  async submit() {
+    if (this.credentials.invalid) {
+      return;
+    }
+    console.log(this.credentials.value);
+  }
 }
