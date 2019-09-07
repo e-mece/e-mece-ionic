21f44bdf03e92ff3e8c6a3aa682f07cda21372cf
Ahmet Uysal <auysal16@ku.edu.tr>
2019-09-07 23:47:27 +0300

    Added contract models

---
+++ src/contract/index.ts
@@ -0,0 +1,21 @@
+export { UserType } from './enums/user-type.enum';
+
+export { Event } from './models/event.model';
+export { User } from './models/user.model';
+
+export { ChangeEmailRequest } from './request/change-email-request.model';
+export { ChangePasswordRequest } from './request/change-password-request.model';
+export { CreateEventRequest } from './request/create-event-request.model';
+export { LoginRequest } from './request/login-request.model';
+export { ResetPasswordRequest } from './request/reset-password-request.model';
+export { SignupRequest } from './request/signup-request.model';
+export { UpdateEventRequest } from './request/update-event-request.model';
+export { UpdateUserRequest } from './request/update-user-request.model';
+
+export { CreateEventResponse } from './response/create-event-response.model';
+export { GetEventResponse } from './response/get-event-response.model';
+export {
+  GetLeaderboardResponse
+} from './response/get-leaderboard-response.model';
+export { GetUserResponse } from './response/get-user-response.model';
+export { LoginResponse } from './response/login-response.model';
---
+++ src/contract/request/update-event-request.model.ts
@@ -0,0 +1,5 @@
+import { Event } from '../models/event.model';
+
+export class UpdateEventRequest {
+  event: Event;
+}
